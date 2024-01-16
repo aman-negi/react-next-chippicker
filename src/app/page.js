08df855fcb438644cce875ad2e9232f376e0a259
amannegi14 <amannegi1401@gmail.com>
2024-01-16 21:39:10 +0530

    added the component

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,131 @@
-import Image from 'next/image'
+// Add the "use client" directive at the top of the file
+"use client";
 
-export default function Home() {
+import React, { useState } from "react";
+
+
+const InputChips = ({ items }) => {
+  const initialValue = "";
+  
+  const [value, setValue] = useState(initialValue);
+  const [chips, setChips] = useState([]);
+
+  // to filter the items based on the input value
+  const filterItems = (value) => {
+    return items.filter(
+      (item) =>
+        item.toLowerCase().includes(value.toLowerCase()) && !chips.includes(item)
+    );
+  };
+
+  // to add a chip and clear the input value
+  const addChip = (chip) => {
+    setChips((prevChips) => [...prevChips, chip]);
+    setValue("");
+  };
+
+  // to remove a chip and add it back to the items
+  const removeChip = (chip) => {
+    setChips((prevChips) => prevChips.filter((c) => c !== chip));
+  };
+
+  // logic -> if input is blank remove previous chip otherwise remove one character from current value
+  const handleBackspace = () => {
+    if (value === "") {
+      setChips((prevChips) => prevChips.slice(0, -1));
+    } else {
+      setValue((prevValue) => prevValue.slice(0, -1));
+    }
+  };
+
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  // A function to handle the key press for backspace and enter
+  const handleKeyDown = (e) => {
+    if (e.key === "Backspace") {
+      handleBackspace();
+    } else if (e.key === "Enter") {
+      if (filterItems(value).length > 0) {
+        addChip(filterItems(value)[0]);
+      }
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="max-w-3xl">
+      <label
+        htmlFor="chips-input"
+        className="chips-input border-b-4 border-blue-500 p-2 flex flex-wrap gap-2 items-center"
+      >
+        {chips.map((chip) => (
+          <div
+            className="chip inline-flex items-center bg-gray-200 pl-3 pr-1 py-1 rounded-full"
+            key={chip}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            {chip}
+            <span
+              className="close ml-1 cursor-pointer hover:bg-gray-300 hover:shadow-lg rounded-full px-2.5 py-1 "
+              onClick={() => removeChip(chip)}
+            >
+              X
+            </span>
+          </div>
+        ))}
+        <div className="input-list relative z-10">
+          <input
+            type="text"
+            id="chips-input"
+            value={value}
+            onChange={handleChange}
+            onKeyDown={handleKeyDown}
+            placeholder="Type something..."
+            className="w-full h-8 border-0 focus:outline-none"
+          />
+            <div className="list absolute top-full left-0 w-full max-h-44 pl-1.5  pr-2 py-3 bg-white overflow-y-auto border border-gray-300 rounded-md shadow-md">
+            {filterItems(value).map((item) => (
+              <div
+                className="item p-2 hover:bg-gray-200 cursor-pointer"
+                key={item}
+                onClick={() => addChip(item)}
+                tabIndex="0"
+              >
+                {item}
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
+      </label>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    </div>
+  );
+};
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+// A sample list for testing
+const items = [
+  "Nick Giannopoulos",
+  "Mary Coustas",
+  "George Kapiniaris",
+  "Effie Stephanidis",
+  "Alex Dimitriades",
+  "Vince Colosimo",
+  "Claudia Karvan",
+  "Abi Tucker",
+  "Deborah Mailman",
+  "Ryan Corr",
+];
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+// The main component
+const Home = () => {
+  return (
+    <div className="p-20">
+      <center><h1 className="text-3xl font-bold text-blue-500">Pick Users</h1> 
+      <InputChips items={items} /></center>  
+    </div>
+  );
+};
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Home;
